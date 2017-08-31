7c862784cc67c7923399b746be5f4da62e201bc6
MatthewSmickle <matt.smickle.o@gmail.com>
2017-08-31 11:26:47 -0400

    Favorite Food

---
+++ FavoriteFood/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your favorite food? : ");
+            var input = Console.ReadLine();
+            if(input == "pizza")
+            {
+                Console.WriteLine("Great choice");
+            }
+            else
+            {
+                Console.WriteLine("Wrong!  That is a terrible choice and you should feel bad about yourself.   Your favorite should be pizza!");
+
+            }
+            
+            
+            Console.WriteLine(input);
+         }
+    }
+}
