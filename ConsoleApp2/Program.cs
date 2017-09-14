870d15245fd82dd48c871bd20e1325f767a27802
MatthewSmickle <matt.smickle.o@gmail.com>
2017-09-14 11:17:41 -0400

    Palindrome & setup for calc

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an expression: ");
+            var input = Console.ReadLine();
+
+            for(int i = 0; i <= input.Length();i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ GettingStarted/Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i > -1; )
+            {
+
+
+                Console.Write("Enter a phrase: ");
+                var inp = Console.ReadLine();
+
+                var input = inp.ToLower();
+
+                input = input.Replace(" ", "");
+                input = input.Replace("!", "");
+                input = input.Replace(".", "");
+                input = input.Replace(",", "");
+                input = input.Replace(";", "");
+                input = input.Replace(":", "");
+                input = input.Replace("?", "");
+
+                char[] reversed = input.ToCharArray();
+                Array.Reverse(reversed);
+                String rev = new String(reversed);
+
+                if (rev == input)
+                {
+                    Console.WriteLine("\"" + inp + "\" is a palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine("\"" + inp + "\" is not a palindrome.");
+                }
+            }
+
+
+        }
+    }
+}
