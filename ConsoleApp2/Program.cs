1cb7b66e0d4cff7d49a66623ee6ee4419189307d
MatthewSmickle <matt.smickle.o@gmail.com>
2017-09-18 11:32:41 -0400

    SimpleCalc

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,12 +11,55 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.Write("Enter an expression: ");
-            var input = Console.ReadLine();
+            var Input = Console.ReadLine();
 
-            for(int i = 0; i <= input.Length();i++)
+        
+            String FirstNum = "";
+            String SecondNum = "";
+            if (Input.Substring(0,2).Contains(" "))
             {
-
+                FirstNum = Input.Substring(0, 1);
+                if(Input.Substring(Input.Length-2).Contains(" "))
+                {
+                    SecondNum = Input.Substring(Input.Length - 2);
+                }
+                else
+                {
+                     SecondNum = Input.Substring(Input.Length - 3);
+                }
             }
+            else
+            {
+                FirstNum = Input.Substring(0, 2);
+                SecondNum = Input.Substring(Input.Length - 2);
+            }
+
+            int NewFirst = Convert.ToInt32(FirstNum);
+            int NewSecond = Convert.ToInt32(SecondNum);
+
+            
+
+           if(Input.Contains("+"))
+            {
+                double result = NewFirst + NewSecond;
+                Console.WriteLine("Result: "+ Input + " = "+result);
+            }
+            if (Input.Contains("*"))
+            {
+                double result = NewFirst * NewSecond;
+                Console.WriteLine("Result: " + Input + " = " + result);
+            }
+            if (Input.Contains("-"))
+            {
+                double result = NewFirst - NewSecond;
+                Console.WriteLine("Result: " + Input + " = " + result);
+            }
+            if (Input.Contains("/"))
+            {
+                double result = NewFirst / NewSecond;
+                Console.WriteLine("Result: " + Input + " = " + result);
+            }
+
         }
     }
 }
