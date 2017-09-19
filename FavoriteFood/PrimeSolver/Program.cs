f7798702c86ca0819fb81fd955f33e309f6c354d
MatthewSmickle <31512911+MatthewSmickle@users.noreply.github.com>
2017-09-18 23:56:30 -0400

    Trash Attempt at Prime Solver

---
+++ FavoriteFood/PrimeSolver/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transformers . . Roll out!");
+            int notPrime = 0;
+            for(int i = 0; i <= 1000; i++)
+            {
+                for(int num = 0; i == 1000; num++)
+                {
+                    for (int minus = num - 1; minus == 2; minus--)
+                    {
+                        if (num % minus == 0)
+                        {
+                            notPrime++;
+                        }
+
+                        if (notPrime == 0)
+                        {
+                            i++;
+                            Console.WriteLine(i);
+
+                        }
+                    }
+                }
+            }
+
+
+            
+            Console.WriteLine("Done.");
+        }
+    }
+}
